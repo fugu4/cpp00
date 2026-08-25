70508df9e0179df909fd5e6c7d5fb47543dadacc
hnogi <hnogi@student.42tokyo.jp>
2026-08-25 13:17:23 +0900

    to github

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,3 +1,37 @@
 #include <iostream>
 #include "Account.hpp"
 
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+Account::Account (int inital_deposit)
+{
+	
+}
+
+int Account::getNbAccounts(void)
+{
+	return _nbAccounts;
+}
+
+int Account::getTotalAmount(void)
+{
+	return _totalAmount;
+}
+
+int Account::getNbDeposits(void)
+{
+	return _totalNbDeposits;
+}
+
+int Account::getNbWithdrawals(void)
+{
+	return _totalNbWithdrawals;
+}
+
+int	checkAmount( void ) const
+{
+	return _amount;
+}
